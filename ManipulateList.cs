33d4a2f8150af007f27bbb0532b5a46064c1aee9
rounak <rounakkole@gmail.com>
2022-02-24 19:43:26 +0530

    [rounak] ADDED: simple linked list

---
+++ ManipulateList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class ManipulateList
+    {
+
+        public Node head;
+
+        public void Insert()
+        {
+            Node newNode = new Node();
+         
+
+            newNode.data = 56;
+            Node temp = newNode;
+            newNode.data = 30;
+           temp.next = newNode;
+            temp = temp.next;
+            newNode.data = 70;
+            temp.next = newNode;
+            Console.WriteLine(temp.data);
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace LinkedKist
+namespace LinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure");
+            ManipulateList manipulateList = new ManipulateList();
+            manipulateList.Insert();
+       
         }
     }
 }
